a6136d4aaa09ef7c86f5dc25aa63cd62af3dbfaf
PARDEEP KAUR <pkaur84@aol.com>
2020-06-14 07:16:36 +0000

    Automatically backed up by Learn

---
+++ app/models/dog.rb
@@ -0,0 +1,18 @@
+class Dog
+  
+  attr_accessor :name ,:breed,:age
+  
+  @@all =[]
+  
+  def initialise(name,breed,age)
+    @name = name
+    @breed = breed
+    @age = age
+    
+  end
+  
+  def self.all
+    @@all
+  end 
+end
+  
